9bc8879f951f9b3b91f35c0d3a9f6b3d4af17f15
alain <chadnpc@outlook.com>
2026-02-20 01:57:58 +0200

    Create Merge-Pdf.ps1

---
+++ Public/Merge-Pdf.ps1
@@ -0,0 +1,54 @@
+function Merge-Pdf {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [string[]]$FilePaths,
+
+    [Parameter(Mandatory)]
+    [string]$PublicKey,
+
+    [Parameter(Mandatory)]
+    [string]$PrivateKey,
+
+    [Parameter()]
+    [string]$OutputFolder = ".",
+
+    [Parameter()]
+    [string]$OutputFileName = "merged.pdf"
+  )
+
+  begin {
+    $api = [ilovepdfapi]::new($PublicKey, $PrivateKey)
+    $task = $api.CreateTask([MergeTask])
+  }
+
+  process {
+    foreach ($file in $FilePaths) {
+      if (-not (Test-Path $file)) {
+        Write-Error "File not found: $file"
+        continue
+      }
+      Write-Verbose "Uploading file: $file"
+      $null = $task.UploadFile($file)
+    }
+  }
+
+  end {
+    Write-Verbose "Processing merge task on iLovePDF server..."
+    $executionRes = $task.Process()
+
+    Write-Verbose "Downloading merged files to $OutputFolder"
+
+    $downloadDest = Join-Path $OutputFolder $OutputFileName
+
+    $task.DownloadFile($downloadDest)
+
+    Write-Verbose "Task finished. File saved to $downloadDest"
+
+    return [PSCustomObject]@{
+      OutputFilesize   = $executionRes.OutputFileSize
+      OriginalFilesize = $executionRes.FileSize
+      SavedPath        = $downloadDest
+    }
+  }
+}
