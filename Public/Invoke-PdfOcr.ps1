0883d161a9f0f3619e14db2f8359545d768003c3
alain <chadnpc@outlook.com>
2026-02-20 02:04:19 +0200

    Create Invoke-PdfOcr.ps1

---
+++ Public/Invoke-PdfOcr.ps1
@@ -0,0 +1,62 @@
+function Invoke-PdfOcr {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [string[]]$FilePaths,
+
+    [Parameter(Mandatory)]
+    [string]$PublicKey,
+
+    [Parameter(Mandatory)]
+    [string]$PrivateKey,
+
+    [Parameter()]
+    [string]$OutputFolder = ".",
+
+    [Parameter()]
+    [string]$OutputFileName = "ocr_output.pdf",
+
+    [Parameter()]
+    [OCRLanguage[]]$OCRLanguages
+  )
+
+  begin {
+    $api = [ilovepdfapi]::new($PublicKey, $PrivateKey)
+    $task = $api.CreateTask([PdfocrTask])
+  }
+
+  process {
+    foreach ($file in $FilePaths) {
+      if (-not (Test-Path $file)) {
+        Write-Error "File not found: $file"
+        continue
+      }
+      Write-Verbose "Uploading file: $file"
+      $null = $task.UploadFile($file)
+    }
+  }
+
+  end {
+    $params = [PDFOCRParams]::new()
+    if ($OCRLanguages) {
+      $params.OCRLanguages = [List[OCRLanguage]]::new($OCRLanguages)
+    }
+
+    Write-Verbose "Processing OCR task on iLovePDF server..."
+    $executionRes = $task.Process($params)
+
+    Write-Verbose "Downloading OCR files to $OutputFolder"
+
+    $downloadDest = Join-Path $OutputFolder $OutputFileName
+
+    $task.DownloadFile($downloadDest)
+
+    Write-Verbose "Task finished. File saved to $downloadDest"
+
+    return [PSCustomObject]@{
+      OutputFilesize   = $executionRes.OutputFileSize
+      OriginalFilesize = $executionRes.FileSize
+      SavedPath        = $downloadDest
+    }
+  }
+}
